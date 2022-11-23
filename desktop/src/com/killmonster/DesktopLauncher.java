df3aa77a34e13c420800ac8e8ead7640afacc75f
thethanh02 <2002tintin@gmail.com>
2022-11-23 23:06:49 +0700

    sua map

--- desktop/src/com/killmonster/DesktopLauncher.java
+++ desktop/src/com/killmonster/DesktopLauncher.java
@@ -3,14 +3,13 @@ package com.killmonster;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
 
-// Please note that on macOS your application needs to be started with the -XstartOnFirstThread JVM argument
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
 		config.setForegroundFPS(60);
 		config.setTitle("KillMonster");
 		config.useVsync(true);
-		config.setWindowedMode(26*16*3*3/2, 14*16*3*3/2);;
+		config.setWindowedMode(26*16*3*3/2, 14*16*3*3/2);
 		new Lwjgl3Application(new KillMonster(), config);
 	}
 }
